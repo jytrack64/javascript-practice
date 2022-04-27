0f399f332793a712b274058d00119c581063455b
jytrack64 <rlawldus1368@naver.com>
2022-04-27 21:41:28 +0900

    Make a Calculator

---
+++ Calculator/main.js
@@ -0,0 +1,24 @@
+function Calculator() {
+  this.read = function () {
+    input = prompt('두 개의 숫자를 입력하세요. (ex. 2, 1)')
+    inputsplit = input.split(',')
+
+    // parseInt, Number 모두 가능
+    this.input1 = Number(inputsplit[0])
+    this.input2 = Number(inputsplit[1])
+  }
+
+  this.sum = function() {
+    return this.input1 + this.input2
+  }
+
+  this.mul = function() {
+    return this.input1 * this.input2
+  }
+}
+
+let calculator = new Calculator()
+calculator.read()
+
+alert("Sum= " + calculator.sum())
+alert("Mul= " + calculator.mul())
